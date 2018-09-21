d2f6c56c7f74ae2e20d69275603671a1b69d2b30
Nicholas <nicholas.l.hopper@gmail.com>
2018-09-20 19:32:01 -0500

    Adding base spring project

---
+++ src/main/java/works/nicholas/home/thermo/ThermoApplication.java
@@ -0,0 +1,12 @@
+package works.nicholas.home.thermo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThermoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThermoApplication.class, args);
+	}
+}
